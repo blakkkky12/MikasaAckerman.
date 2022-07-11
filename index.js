0c8891891020330180aa4472060b8d80d7acbfbe
blakkkky12 <108606315+blakkkky12@users.noreply.github.com>
2022-07-11 23:00:43 +0700

    update index.js

--- index.js
+++ index.js
@@ -51,17 +51,17 @@ console.log(color(figlet.textSync('Cheems Bot-MD', {
 	    width: 80,
 		whitespaceBreak: true
         }), 'yellow'))
-console.log(color('\n🐶 YT CHANNEL:','silver'))
-console.log(color('🐶 GITHUB:','silver'))
-console.log(color('🐶 WA NUMBER: +24105166540','silver'))
+console.log(color('\n😺 INSTAGRAM:','silver'))
+console.log(color('😺 GITHUB:','silver'))
+console.log(color('😺 WA NUMBER: +6281365681189','silver'))
 console.log(color('Hendraa','mediumseagreen'))
-    console.log(color('🐶','red'), color('I Wrote This Script By Myself!', 'yellow'))
-    console.log(color('🐶','red'), color('Source Code Version: 3.0', 'aqua'))
-    console.log(color('🐶','red'), color('Bug? Error? Suggestion? Talk to developer:', 'aqua'))
-    console.log(color('🐶', 'cyan'), color('https://wa.me/916909137213'))
-    console.log(color('🐶', 'cyan'), color('Cheems Bot Is Online...', 'pink'))
-    console.log(color('🐶', 'cyan'), color('Welcome Back Owner! Hope You Doing Well~', 'magenta'))
-    console.log(color('🐶','red'), color('Thanks For Using Cheems Bot', 'white'))
+    console.log(color('😺','red'), color('I Wrote This Script By Myself!', 'yellow'))
+    console.log(color('😺','red'), color('Source Code Version: 3.0', 'aqua'))
+    console.log(color('😺','red'), color('Bug? Error? Suggestion? Talk to developer:', 'aqua'))
+    console.log(color('😺', 'cyan'), color('https://wa.me/916909137213'))
+    console.log(color('😺', 'cyan'), color('Mikasa Bot Is Online...', 'pink'))
+    console.log(color('😺', 'cyan'), color('Welcome Back Owner! Hope You Doing Well~', 'magenta'))
+    console.log(color('😺','red'), color('Thanks For Using Cheems Bot', 'white'))
     let { version, isLatest } = await fetchLatestBaileysVersion()
     const XeonBotInc = XeonBotIncConnect({
         logger: pino({ level: 'silent' }),
