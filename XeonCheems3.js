b5aceea390727bb9600d6dcc06f2418cd9615f9f
blakkkky12 <108606315+blakkkky12@users.noreply.github.com>
2022-07-11 19:37:02 +0700

    update XeonCheems3.js

--- XeonCheems3.js
+++ XeonCheems3.js
@@ -695,8 +695,8 @@ const order = generateWAMessageFromContent(jid, proto.Message.fromObject({
 "itemCount": itcount, // Change the Item Count
 "status": "INQUIRY", // Don't Replace
 "surface": "CATALOG", // Don't Replace
-"orderTitle": Mikasa Ackerman, // Change the title
-"message": Botz, // Change Message
+"orderTitle": mikasa ackerman, // Change the title
+"message": botz, // Change Message
 "sellerJid": sellers, // Change the seller
 "token": tokens, // Change the token
 "totalAmount999999": ammount, // Change the Total Amount
